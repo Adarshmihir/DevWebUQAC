ea13d45e460bc52a515a0d220dcff00daedf7f34
FarisAjeddig <fajeddig@hotmail.fr>
2019-03-13 18:04:43 +0100

    class user ok (with preference, phonenumber, type & const)

--- TP3/src/AppBundle/Entity/User.php
+++ TP3/src/AppBundle/Entity/User.php
@@ -20,6 +20,22 @@ class User extends BaseUser
     const DRIVER_TYPE = "Driver";
     const PASSENGER_TYPE = "Passenger";
 
+    const SMOKE_AUTHORIZED = "Smoke is authorized";
+    const ACCESS_PHONENUMBER = "Access to phonenumber";
+    const ACCESS_MAIL = "Access to mail";
+    const CONDITIONING_AIR = "Conditioning air";
+
+    const ANIMALS = "animals";
+    const ANIMALS_IN_CAGE = "animals in cage";
+    const NO_ANIMALS = "no animals";
+    const FREE_ANIMALS = "free animals";
+    const INDIFFERENT_ANIMALS = "indifferent for animals";
+
+    const BIKE_RACK="bike rack";
+    const SKI_RACK="ski rack";
+
+    //à voir pour le type de véhicule parce qu'on va pas lister tous les possibles
+
     /**
      * @ORM\Id
      * @ORM\Column(type="integer")
@@ -38,4 +54,70 @@ class User extends BaseUser
      * @ORM\Column(type="string", name="type")
      */
     protected $type=User::PASSENGER_TYPE;
+
+    /**
+     * @ORM\Column(type="array", name="preference")
+     */
+    protected $preference=[
+        User::SMOKE_AUTHORIZED => false,
+        self::ACCESS_PHONENUMBER => false,
+        self::ACCESS_MAIL => false,
+        self::CONDITIONING_AIR => false,
+        self::ANIMALS => self::NO_ANIMALS,
+        self::BIKE_RACK => false,
+        self::SKI_RACK => false
+        ];
+
+    /**
+     * @ORM\Column(type="phone_number")
+     */
+    protected $phoneNumber;
+
+    /**
+     * @return mixed
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param mixed $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPreference()
+    {
+        return $this->preference;
+    }
+
+    /**
+     * @param mixed $preference
+     */
+    public function setPreference($preference)
+    {
+        $this->preference = $preference;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhoneNumber()
+    {
+        return $this->phoneNumber;
+    }
+
+    /**
+     * @param mixed $phoneNumber
+     */
+    public function setPhoneNumber($phoneNumber)
+    {
+        $this->phoneNumber = $phoneNumber;
+    }
 }
